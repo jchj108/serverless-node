4cfbb26ead5ad64f3f5921f152ddb63d4a6c8f15
jin <jchj108@naver.com>
2023-11-07 01:30:36 +0900

    serverless.yml, swagger

--- src/functions/hello/handler.ts
+++ src/functions/hello/handler.ts
@@ -4,7 +4,7 @@ import { middyfy } from '@libs/lambda';
 
 import schema from './schema';
 
-const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
+export const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
   return formatJSONResponse({
     message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
     event,
